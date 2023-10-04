2e3f38c96880ce0aed4ca566ffdfef39e59a6fd8
Itsuki Hashizume <2111140089u@ed.fuk.kindai.ac.jp>
2023-10-04 06:59:22 +0000

    コードの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Twitter_production"
+  # config.active_job.queue_name_prefix = "SWEXE_L3_production"
 
   config.action_mailer.perform_caching = false
 
--- db/migrate/20231002055331_create_tweets.rb
+++ db/migrate/20231002055331_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
       t.string :message
+      t.datetime :tdate
 
       t.timestamps
     end
